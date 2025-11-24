92739baf3df611a4d324a3038a2761a8baefa57f
Jeferson <luiz_cleidi@hotmail.com>
2025-11-24 14:55:25 -0300

    feat: icons modaltech

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,19 +13,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/icon_modaltech.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/icon_modaltech.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/icon_modaltech_simbolo.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/icon_modaltech.png',
   },
 }
 
